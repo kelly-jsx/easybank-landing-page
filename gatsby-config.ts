8af53f407d7c9040fe675f903acaf6f619c4f173
kelly-jsx <seoch.gamer@gmail.com>
2023-03-24 12:01:22 +0100

    first commit

---
+++ gatsby-config.ts
@@ -0,0 +1,51 @@
+import type { GatsbyConfig } from 'gatsby'
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Easybank landing page`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-postcss',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/favicon-32x32.png',
+      },
+    },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Public Sans`,
+            file: `https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;700&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
+}
+
+export default config
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          'dark-blue': 'hsl(233, 26%, 24%)',
+          'lime-green': 'hsl(136, 65%, 51%)',
+          'bright-cyan': 'hsl(192, 70%, 51%)',
+        },
+        neutral: {
+          'grayish-blue': 'hsl(233, 8%, 62%)',
+          'light-grayish-blue': 'hsl(220, 16%, 96%)',
+          'very-light-gray': 'hsl(0, 0%, 98%)',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
