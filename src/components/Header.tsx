ec023eb795bb826d9362e65ae4e2d2b2cc5821ca
kelly-jsx <seoch.gamer@gmail.com>
2023-03-25 12:39:25 +0100

    simple header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../images/logo.svg'
+import hamburger from '../images/icon-hamburger.svg'
+// import Img from 'gatsby-image'
+
+export const Header = () => {
+  return (
+    <>
+      <header className="flex justify justify-between p-4 bg-white">
+        <img src={logo} alt="" />
+        <img src={hamburger} alt="" />
+      </header>
+    </>
+  )
+}
